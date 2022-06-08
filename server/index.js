ee8ac516b45315a487976472bd9b13825d92eecf
Yonatan Haddish <yonihailshn@gmail.com>
2022-06-08 14:20:45 -0400

    blackfrost tutorial

--- server/index.js
+++ server/index.js
@@ -1,11 +1,34 @@
 const express = require("express");
+const Blockfrost = require("@blockfrost/blockfrost-js");
 const app = express();
+const API = new Blockfrost.BlockFrostAPI({
+    projectId: "testnetOZvrsaEPH8PLjH9XVVgtzK8TXJYnbvsA",
+});
 
-function runExample () {
+
+function runExample01 () {
     app.listen(3001, () => {
         console.log("Server Running!");
     });
 };
 
-runExample();
+async function runExample02() {
+    try {
+        const latestBlock = await API.blocksLatest();
+        const networkInfo = await API.network();
+        const latestEpoch = await API.epochsLatest();
+        const health = await API.health();
+
+        console.log("latestBlock", latestEpoch);
+    }
+    catch (err) {
+        console.log("error", err);
+    }
+    
+}
+
+
+runExample02();
+
+
 
