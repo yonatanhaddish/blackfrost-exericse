aeed867af1e41520ef84e0789cf140a399f32e1e
Yonatan Haddish <yonihailshn@gmail.com>
2022-06-09 13:25:24 -0400

    blackfrost tutorial

--- server/blockfrost.controller.js
+++ server/blockfrost.controller.js
@@ -15,8 +15,10 @@ let requestOption = {
 const getScriptUtxo = async (req, params) => {
     
     try {
-        const address_mine = "addr_test1qqdtqvzkl3fd0vrtjy3z40a700s8j0q0q5g9dcucdwmwk5nwy6f7gh2vzugvl7cwyhp5nsmrp4qj7ypf9wku3kc9e2hszrn7n5"
-        const api_url = `https://cardano-testnet.blockfrost.io/api/v0/addresses/${address_mine}/utxos`
+        const stake_address = "stake_test1uphzdylyt4xpwyx0lv8zts6fcd3s6sf0zq5jhtwgmvzu4tcekrgmh"
+        const address_01 = "addr_test1qqdtqvzkl3fd0vrtjy3z40a700s8j0q0q5g9dcucdwmwk5nwy6f7gh2vzugvl7cwyhp5nsmrp4qj7ypf9wku3kc9e2hszrn7n5"
+        const address_02 = "addr_test1qzu3ccl3k4m9e6c9wag009fldnmj2fu8vnggaw8qter7z5hd6ksrht9dann3wpk0qgrme60w779980cvfexa7slg3n0qd4zxj2"
+        const api_url = `https://cardano-testnet.blockfrost.io/api/v0/addresses/${address_02}`
         const fetch_response = await fetch(api_url, requestOption);
         const fetch_json = await fetch_response.json();
         console.log(fetch_json);
