663911271b0f3d19ecefbd1acc7e6e0bd4f3d8ab
Yonatan Haddish <yonihailshn@gmail.com>
2022-06-17 14:02:25 -0400

    blockfrost tutotial on querying data

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 
 app.use('/', require("./routes/indexRoutes"));
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
