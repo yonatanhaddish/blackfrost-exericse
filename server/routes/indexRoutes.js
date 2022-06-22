ea4756d08ded2727daff53924ced225159748ee8
Yonatan Haddish <yonihailshn@gmail.com>
2022-06-22 17:14:04 -0400

    api from blockfrost

--- server/routes/indexRoutes.js
+++ server/routes/indexRoutes.js
@@ -7,7 +7,7 @@ indexController = require('../controllers/indexController');
 router.get('/api/transaction/:id', indexController.transactionIdUtxos);
 router.get('/api/account/:id', indexController.accountInfo);
 router.get('/api/address/:id', indexController.getScriptUtxo);
-// router.get('/api/test/:id', indexController.getInfoTest);
+router.get('/api/test/:id', indexController.getInfoTest);
 // router.post('/api/transaction', indexController.createTransaction);
 
 
