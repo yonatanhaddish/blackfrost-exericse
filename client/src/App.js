d7a5aaaebc5168d1e40c0a4ac47f9a640e9e288c
Yonatan Haddish <yonihailshn@gmail.com>
2022-06-29 12:01:01 -0400

    walletSelected handler on client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+
+import {
+  Address,
+  BaseAddress,
+  MultiAsset,
+  Assets,
+  ScriptHash,
+  Costmdls,
+  Language,
+  CostModel,
+  AssetName,
+  TransactionUnspentOutput,
+  TransactionUnspentOutputs,
+  TransactionOutput,
+  Value,
+  TransactionBuilder,
+  TransactionBuilderConfigBuilder,
+  TransactionOutputBuilder,
+  LinearFee,
+  BigNum,
+  BigInt,
+  TransactionHash,
+  TransactionInputs,
+  TransactionInput,
+  TransactionWitnessSet,
+  Transaction,
+  PlutusData,
+  PlutusScripts,
+  PlutusScript,
+  PlutusList,
+  Redeemers,
+  Redeemer,
+  RedeemerTag,
+  Ed25519KeyHashes,
+  ConstrPlutusData,
+  ExUnits,
+  Int,
+  NetworkInfo,
+  EnterpriseAddress,
+  TransactionOutputs,
+  hash_transaction,
+  hash_script_data,
+  hash_plutus_data,
+  // index_of_input,
+  ScriptDataHash,
+  Ed25519KeyHash,
+  NativeScript,
+  StakeCredential,
+} from "@emurgo/cardano-serialization-lib-asmjs";
+
+// let Buffer = require('buffer/').Buffer
 
 import WalletConnector from "./components/WalletConnector";
 
--- client/src/components/WalletConnector.js
+++ client/src/components/WalletConnector.js
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-function WalletConnector() {
+import { Radio, RadioGroup } from "@blueprintjs/core"
+
+const WalletConnector = () => {
+
+  const [whichWalletSelected, setwhichWalletSelected] = useState('');
+  const [wallets1, setwallets1] = useState([]);
+  const [walletIsEnabled, setwalletIsEnabled] = useState(false);
+
+  const handleWalletSelect = (obj) => {
+    const whichWalletSelected = obj.target.value
+    setwhichWalletSelected(whichWalletSelected);
+    console.log(whichWalletSelected);
+  };
+  
+
   return (
     <>
-        <div>WalletConnector</div>
-        <p>Testing</p>
+      <div>
+        <RadioGroup label= "Select Wallet:"
+                    onChange={handleWalletSelect}
+         >
+          <Radio label='Nami' value='nami' />
+          <Radio label='Eternl' value='eternl' />
+          <Radio label='Yoroi' value='yoroi' />
+        </RadioGroup>
+      </div>
     </>
-    
-  );
+  )
 };
 
-export default WalletConnector
+export default WalletConnector;
