61e7b964e274e4df62c39d44e9cdc11113846f4d
Yonatan Haddish <yonihailshn@gmail.com>
2022-06-29 13:06:25 -0400

    walletSelected handler on client side

--- client/src/components/WalletConnector.js
+++ client/src/components/WalletConnector.js
@@ -5,14 +5,43 @@ import { Radio, RadioGroup } from "@blueprintjs/core"
 const WalletConnector = () => {
 
   const [whichWalletSelected, setwhichWalletSelected] = useState('');
-  const [wallets1, setwallets1] = useState([]);
-  const [walletIsEnabled, setwalletIsEnabled] = useState(false);
+  
 
   const handleWalletSelect = (obj) => {
     const whichWalletSelected = obj.target.value
     setwhichWalletSelected(whichWalletSelected);
-    console.log(whichWalletSelected);
+    // console.log(whichWalletSelected);
+    refreshData();
   };
+
+  const checkIfWalletFound = () => {
+    let walletIsFound = false;
+
+    const wallet = whichWalletSelected;
+    if (wallet == 'nami') {
+      console.log("nami")
+    }
+    else if (wallet == "eternl") {
+      console.log("eternl")
+    }
+    else if (wallet == 'yoroi') {
+      console.log('yoroi')
+    }
+    return walletIsFound;
+  }
+
+  const refreshData = async () => {
+    try {
+      const walletIsFound = checkIfWalletFound();
+      if (walletIsFound) {
+        console.log("walletFound")
+      }
+      else console.log("WalletNotFound");
+    }
+    catch (err) {
+      console.log(err)
+    }
+  }
   
 
   return (
@@ -20,6 +49,7 @@ const WalletConnector = () => {
       <div>
         <RadioGroup label= "Select Wallet:"
                     onChange={handleWalletSelect}
+                    selectedValue={whichWalletSelected}
          >
           <Radio label='Nami' value='nami' />
           <Radio label='Eternl' value='eternl' />
